bf09b2046d245fc54b481b8b4af7fa01254a26f2
Stratehm <stratehm@hotmail.com>
2015-06-17 14:05:03 +0200

    Restore the GUI compatibility with Internet Explorer

--- src/main/resources/webapp/js/main.js
+++ src/main/resources/webapp/js/main.js
@@ -39,7 +39,6 @@ require.config({
 
 	'ui': '../ui',
 	'templates': '../ui/templates',
-	'controllers': 'controllers',
 	'locales': '../locales'
     },
     shim: {
@@ -112,3 +111,30 @@ Array.prototype.removeItem = function(item) {
 	return deleted[0];
     }
 };
+
+// IE CustomEvent polyfill
+(function () {
+    'use strict';
+    function CustomEvent (event, params) {
+        params = params || { bubbles: false, cancelable: false, detail: undefined };
+        var evt;
+        try {
+            evt = document.createEvent('CustomEvent');
+            evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+        } catch (error) {
+            // fallback for browsers that don't support createEvent('CustomEvent')
+            evt = document.createEvent('Event');
+            for (var param in params) {
+                evt[param] = params[param];
+            }
+            evt.initEvent(event, params.bubbles, params.cancelable);
+        }
+        return evt;
+    }
+
+    if(typeof window.CustomEvent !== 'undefined') {
+        CustomEvent.prototype = window.CustomEvent.prototype;
+    }
+
+    window.CustomEvent = CustomEvent;
+})();
